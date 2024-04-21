b991a5236e58c91f881a229d4c2c46c857bd41ba
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-21 15:07:11 +0530

    code of backend

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const port = 8080;
 app.listen(port, ()=>{
     console.log(`app is listening on port ${port}`);
 });
+
+app.use((req, res) =>{
+    console.log(req);
+    console.log("req accepted");
+});
