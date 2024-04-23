36ff85b1a663ba8ed839e2e1cedd3f673a313c05
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-23 18:06:08 +0530

    backend code

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res)=>{
     res.send("Hello i am root");
 });
 
-app.get("/:username", (req, res) => {
+app.get("/:username/:id", (req, res) => {
     console.log(req.params);
     res.send("Hello");
 });
