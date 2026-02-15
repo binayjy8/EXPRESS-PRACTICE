81b6b359441816a92354381f1a46287ada5da0a7
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-02-15 09:12:41 +0530

    web update

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/search", (req, res) => {
 });
 
 // app.get("/search", (req, res)=>{
-//     res.send("you connect with search pt");
+//     res.send("you connect with search t");
 // });
 
 // app.get("/help", (req, res)=>{
