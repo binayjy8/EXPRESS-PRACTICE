4f4fa8c96a391986d2ff16d957e397f91f29c0ac
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-02-15 22:52:38 +0530

    express code

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/search", (req, res) => {
 });
 
 // app.get("/search", (req, res)=>{
-//     res.send("you connect with");
+//     res.send("you connect wit");
 // });
 
 // app.get("/help", (req, res)=>{
