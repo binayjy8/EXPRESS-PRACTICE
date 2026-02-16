0af85b4138245f05160a2190fc481d4066557959
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-02-16 20:59:57 +0530

    express code

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/search", (req, res) => {
 // });
 
 // app.get("*", (req, res)=>{
-//     res.send("path does not xit");
+//     res.send("path does not xi");
 // });
 
 // app.use((req, res) =>{
