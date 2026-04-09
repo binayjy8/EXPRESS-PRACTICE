f9efee0b824752860adf41226e865f549eef1a47
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-04-09 19:58:52 +0530

    express

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ app.get("/search", (req, res) => {
 // app.use((req, res) =>{
 //     // console.log(req);
 //     console.log("req");
-//     let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>Mango</li></ul>";
+//     let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>Mano</li></ul>";
 //     res.send();
 // });
